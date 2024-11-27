553246b500014aaed459cd59138b4f4eb8ff3d36
chlovers <chloie_day@hotmail.com>
2024-11-27 11:30:10 -0400

    added enemy spawn script

---
+++ Assets/scripts/Enemy Spawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public MapGenerator mapGenerator;
+    public GameObject Ghost;
+    public GameObject Mimic;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
