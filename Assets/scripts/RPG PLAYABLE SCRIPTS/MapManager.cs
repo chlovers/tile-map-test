f0fa50ce12b83ad21a867763b6080d70d2240606
chlovers <chloie_day@hotmail.com>
2024-12-04 00:10:19 -0400

    .

---
+++ Assets/scripts/RPG PLAYABLE SCRIPTS/MapManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapManager : MonoBehaviour
+{
+
+    private Tilemap tilemap;
+    public int Width;
+    public int Height;
+    public Tile[] floortiles;
+    public Tile[] walltiles;
+    private CellData[,] tiledata;
+    public Grid grid;
+    public PlayerController player;
+
+
+    public class CellData
+    {
+        public bool passable;
+    }
+
+     void Start()
+    {
+        tilemap = GetComponentInChildren<Tilemap>();
+       grid = GetComponentInChildren<Grid>();
+        
+        tiledata = new CellData[Width, Height];
+
+        for (int y = 0; y < Height; y++)
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                Tile tile;
+                tiledata[x, y] = new CellData();
+
+                if (x == 0 || y == 0 || x == Width -1 || y == Height -1)
+                {
+                    tile = walltiles[Random.Range(0, walltiles.Length)];
+                    tiledata[x, y].passable = false;
+                }
+            
+                else
+                {
+                    tile = floortiles[Random.Range(0, floortiles.Length)];
+                    tiledata[x, y].passable = true;
+                }
+
+                tilemap.SetTile(new Vector3Int(x,y,0), tile);
+
+            }  
+        }
+        player.Spawn(this, new Vector2Int(1,1));
+    }
+
+    public Vector3 CellToWorld(Vector2Int cellIndex)
+    {
+        return grid.GetCellCenterWorld((Vector3Int)cellIndex);
+    }
+
+
+    public CellData GetCellData(Vector2Int cellIndex)
+    {
+        if (cellIndex.x < 0 || cellIndex.x >= Width || cellIndex.y < 0 || cellIndex.y >= Height)
+        {
+            return null;
+        }
+
+        return tiledata[cellIndex.x, cellIndex.y];
+    }
+
+}
--- Assets/scripts/RPG PLAYABLE SCRIPTS/PlayerController.cs
+++ Assets/scripts/RPG PLAYABLE SCRIPTS/PlayerController.cs
@@ -3,37 +3,43 @@ using System.Collections.Generic;
 using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Tilemaps;
 
-public class PlayerSpawner : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    private SimpleMap board;
+   private MapManager map;
     private Vector2Int cellspot;
-    
-    
-    public void Spawn(SimpleMap map,Vector2Int cell)
-    {
-        board = map;
-        MoveTo(cell);
-    }
 
+    public void Spawn(MapManager mapManager, Vector2Int cell)
+    {
+        map = mapManager;
+        MoveTo(cell);
+
+        
+
+    }
+    
     public void MoveTo(Vector2Int cell)
     {
         cellspot = cell;
-        transform.position = board.CellToWorld(cellspot);
+        transform.position = map.CellToWorld(cell);
     }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
-        
         Vector2Int newCell = cellspot;
         bool hasmoved = false;
 
-        if(Keyboard.current.wKey.wasPressedThisFrame)
+        if (Keyboard.current.wKey.wasPressedThisFrame)
         {
             newCell.y += 1;
             hasmoved = true;
+
         }
         else if (Keyboard.current.sKey.wasPressedThisFrame)
         {
@@ -50,11 +56,12 @@ public class PlayerSpawner : MonoBehaviour
             newCell.x -= 1;
             hasmoved = true;
         }
-        if (hasmoved == true)
-        { 
-            SimpleMap.CellData celldata = board.GetCellData(newCell);
 
-            if (celldata != null && celldata.Passible)
+        if (hasmoved) 
+        {
+            MapManager.CellData cellData = map.GetCellData(newCell);
+
+            if (cellData != null && cellData.passable)
             {
                 MoveTo(newCell);
             }
